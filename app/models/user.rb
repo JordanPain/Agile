8688fb8373a19139ae94feea1d9e71eb22ab1150
asplunda <azuzuvsfry@hotmail.com>
2015-01-31 09:49:46 -0800

    Added validation to signup form and set form to save the rest of the user information on signup.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  validates_presence_of :userName, :firstName, :lastName, :birthdate, :city, :state, :zip
+  validates_presence_of :gender, message: "must be selected."
+  validates_uniqueness_of :userName
+  validates_numericality_of :zip
+  validates_length_of :zip, is: 5, message: "must be 5 digits."
 
 end
